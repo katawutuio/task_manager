ecc414e63b19cab0e984a15fa4eb35670c9d4d89
Katawut <katawut.u@sabuysolutions.com>
2023-03-02 17:38:21 +0700

    setup express Router

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('<h1>Task Manager API</h1>');
 });
+
+// routes
+app.use('/api/v1/tasks', tasks);
 
 const port = 5000;
 app.listen(port, console.log(`Server start on port ${port}...`));
