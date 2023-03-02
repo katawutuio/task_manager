b6f1f259e91fe82f18756134b99b66051c6e67e6
Katawut <katawut.u@sabuysolutions.com>
2023-03-02 17:48:53 +0700

    setup controller

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getAllTasks
+} = require('../controllers/tasks');
 
-router.route('/').get((req, res) => {
-    res.send('all item');
-});
+router.route('/').get(getAllTasks);
 
 module.exports = router;
