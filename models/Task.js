5af8173ca06bb7cf2a2d46b4864566a7f64c9b4c
Katawut <katawut.u@sabuysolutions.com>
2023-03-03 16:24:20 +0700

    setup validation, default vaue in mongoose

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    isCompleted: Boolean
+    name: {
+        type: String,
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name can not be more than 20 characters'],
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
