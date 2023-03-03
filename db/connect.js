b4b0aa7b3612ba1f2d9ae2a6a8e545b753d19aff
Katawut <katawut.u@sabuysolutions.com>
2023-03-03 11:08:05 +0700

    use .env file to keep secret string and use dotenv to variable from .env to process.env

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://user1:User1Password@nodeexpressprojects.1ixramy.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority';
-
 const connectDB = (url) => {
     return mongoose.connect(url); // return promise
 }
