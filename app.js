3c6fec1e0b8b4e60ceb6db8ebfb5f8fca137deff
Katawut <katawut.u@sabuysolutions.com>
2023-03-03 10:55:19 +0700

    connect db in app.js and use async to try to connect database before run the server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 // middleware
 app.use(express.json());
@@ -13,4 +14,14 @@ app.get('/', (req, res) => {
 app.use('/api/v1/tasks', tasks);
 
 const port = 5000;
-app.listen(port, console.log(`Server start on port ${port}...`));
+
+const start = async () => {
+    try {
+        await connectDB();
+        app.listen(port, console.log(`Server start on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
