06329d6d144e4e61a417be36df2af29613d392d7
Katawut <katawut.u@sabuysolutions.com>
2023-03-07 13:47:16 +0700

    delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -38,8 +38,19 @@ const updateTask = (req, res) => {
     res.send('update task');
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task');
+const deleteTask = async(req, res) => {
+    try {
+        const {id: taskId} = req.params;
+        const task = await Task.findOneAndDelete({_id: taskId});
+
+        if (!task) {
+            return res.status(404).json({message: `No task with id ${taskId}`});
+        }
+
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
 }
 
 module.exports = {
